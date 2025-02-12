bc67bbd67c4a878a8d9f0e29786522c4685f79b5
Aniket Saha <aniketsahaworkspace@gmail.com>
2025-02-12 20:12:19 +0530

    changes

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -31,13 +31,13 @@ const Username = async ({ params }) => {
             <div className="container px-5 py-24 mx-auto">
               <div className="flex flex-col text-center w-full mb-20">
                 <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
-                  Master Cleanse Reliac Heirloom
+                  Our Happy Customers
                 </h1>
                 <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
-                  Whatever cardigan tote bag tumblr hexagon brooklyn
-                  asymmetrical gentrify, subway tile poke farm-to-table. Franzen
-                  you probably haven't heard of them man bun deep jianbing
-                  selfies heirloom.
+                Our customers love the convenience and ease of our platform. 
+                  From seamless transactions to excellent customer support, 
+                  we strive to provide the best experience for our users. 
+                  Join our community and see why so many people trust us with their financial needs.
                 </p>
               </div>
               <div className="flex flex-wrap -m-4">
